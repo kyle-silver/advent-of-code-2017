35e5167313e01c7462ab065e67e7695e0bfb67ba
Kyle Silver <kyleAsilver@gmail.com>
2021-05-23 14:43:22 -0400

    day 9 part 2

--- tests/day09.rs
+++ tests/day09.rs
@@ -1,12 +1,12 @@
+const RAW: &str = include_str!("res/09.txt");
+
 #[derive(Debug)]
 struct Garbage<'a>(&'a [char]);
 
 impl Garbage<'_> {
     /// assumes that the first character is always '<'
     fn parse(input: &[char]) -> Garbage {
-        // println!("parsing garbage: {:?}", &input[..5]);
         let mut escaped = false;
-        // let mut escaped_duration = 0;
         for (index, c) in input.iter().enumerate() {
             if *c == '!' {
                 escaped = !escaped;
@@ -25,6 +25,23 @@ impl Garbage<'_> {
     fn size_chars(&self) -> usize {
         self.0.len()
     }
+
+    fn valid_chars(&self) -> u32 {
+        let mut count = 0;
+        let mut escaped = false;
+        // let mut escaped_duration = 0;
+        for &c in self.0.iter().skip(1).take(self.0.len() - 2) {
+            if c == '!' {
+                escaped = !escaped;
+                continue;
+            } else if escaped {
+                escaped = false;
+                continue;
+            }
+            count += 1;
+        }
+        return count;
+    }
 }
 
 #[derive(Debug)]
@@ -42,7 +59,6 @@ impl Element<'_> {
     }
 
     fn parse(input: &[char], depth: usize) -> Element {
-        // println!("element input: {:?}", &input[..5]);
         match input[0] {
             '{' => Element::Group(Group::parse(input, depth + 1)),
             '<' => Element::Garbage(Garbage::parse(input)),
@@ -57,6 +73,13 @@ impl Element<'_> {
         match self {
             Element::Group(g) => g.score(depth),
             Element::Garbage(g) => 0
+        }
+    }
+
+    fn count_garbage_chars(&self) -> u32 {
+        match self {
+            Element::Group(g) => g.count_garbage_chars(),
+            Element::Garbage(g) => g.valid_chars(),
         }
     }
 }
@@ -92,14 +115,19 @@ impl Group<'_> {
     fn score(&self, depth: u32) -> u32 {
         depth + self.0.iter().map(|e| e.score(depth + 1)).sum::<u32>()
     }
+
+    fn count_garbage_chars(&self) -> u32 {
+        self.0.iter().map(Element::count_garbage_chars).sum()
+    }
 }
 
 #[test]
 fn part1() {
-    let raw = include_str!("res/09.txt");
-    let input: Vec<_> = raw.chars().collect();
+    let input: Vec<_> = RAW.chars().collect();
     let group = Group::parse(&input, 1);
-    println!("Day 9, part 1: {}", group.score(1));
+    let ans = group.score(1);
+    println!("Day 9, part 1: {}", ans);
+    assert_eq!(9662, ans);
 }
 
 #[test]
@@ -126,4 +154,13 @@ fn test_score() {
     assert_eq!(9, Group::parse(&input, 1).score(1));
     let input = "{{<a!>},{<a!>},{<a!>},{<ab>}}".chars().collect::<Vec<char>>();
     assert_eq!(3, Group::parse(&input, 1).score(1));
+}
+
+#[test]
+fn part2() {
+    let input: Vec<_> = RAW.chars().collect();
+    let group = Group::parse(&input, 1);
+    let ans = group.count_garbage_chars();
+    println!("Day 9, part 2: {}", ans);
+    assert_eq!(4903, ans);
 }
