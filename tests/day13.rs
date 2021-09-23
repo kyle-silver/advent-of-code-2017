61c983e0b0008a7e16994cf200a9d74d8b8b5970
Kyle Silver <kyleAsilver@gmail.com>
2021-09-22 22:02:50 -0400

    edge case for day 13 part 2
    getting caught on day 0 has zero severity, but disqualifies the run

--- tests/day13.rs
+++ tests/day13.rs
@@ -1,48 +1,22 @@
 const INPUT: &str = include_str!("res/13.txt");
 
 #[derive(Debug, Clone)]
-enum Direction {
-    Out,
-    Back,
-}
-
-#[derive(Debug, Clone)]
 struct Scanner {
-    range: usize,
-    position: usize,
-    direction: Direction,
+    range: u32,
 }
 
 impl Scanner {
-    fn new(range: usize) -> Scanner {
-        Scanner {
-            range,
-            position: 0,
-            direction: Direction::Out,
-        }
+    fn new(range: u32) -> Scanner {
+        Scanner { range }
     }
 
-    fn update(&mut self) {
-        if matches!(self.direction, Direction::Out) && self.position == self.range - 1 {
-            self.direction = Direction::Back;
-        } else if matches!(self.direction, Direction::Back) && self.position == 0 {
-            self.direction = Direction::Out;
-        }
-        match self.direction {
-            Direction::Out => self.position += 1,
-            Direction::Back => self.position -= 1,
-        }
-    }
-
-    fn alert(&self) -> bool {
-        // self.position == 0 && matches!(self.direction, Direction::Out)
-        self.position == 0
+    fn alert(&self, picosecond: u32) -> bool {
+        picosecond % ((self.range * 2) - 2) == 0
     }
 }
 
 #[derive(Debug)]
 struct Firewall {
-    // path: HashMap<usize, Scanner>,
     path: Vec<Option<Scanner>>,
 }
 
@@ -56,7 +30,7 @@ impl Firewall {
                 (depth, Scanner::new(range))
             })
             .collect();
-        let capacity = scanners.iter().map(|(index, _)| *index).max().unwrap_or(0);
+        let capacity = scanners.iter().map(|(index, _)| *index).max().unwrap();
         let mut path = vec![None; capacity + 1];
         for (index, scanner) in scanners {
             path[index] = Some(scanner);
@@ -64,72 +38,46 @@ impl Firewall {
         Firewall { path }
     }
 
-    fn update(&mut self) {
-        self.path
-            .iter_mut()
-            .filter_map(|s| s.as_mut())
-            .for_each(|s| s.update())
-    }
-
-    fn size(&self) -> usize {
-        self.path.len()
-    }
-
-    fn score(&self, depth: usize) -> u32 {
-        if let Some(Some(scanner)) = self.path.get(depth) {
-            if scanner.alert() {
-                return (depth * scanner.range) as u32;
-            }
-        }
-        return 0;
+    fn score(&self, start: u32) -> u32 {
+        let score: u32 = self
+            .path
+            .iter()
+            .enumerate()
+            .filter_map(|(i, s)| match s {
+                Some(s) => Some((i, s)),
+                None => None,
+            })
+            .map(|(i, s)| match s.alert(start + i as u32) {
+                true => i as u32 * s.range,
+                false => 0,
+            })
+            .sum();
+        let caught_on_day_1 = self
+            .path
+            .get(0)
+            .unwrap()
+            .as_ref()
+            .map(|s| match s.alert(start) {
+                true => 1,
+                false => 0,
+            })
+            .unwrap_or(0);
+        return score + caught_on_day_1;
     }
 }
 
 #[test]
 fn part1() {
-    let mut firewall = Firewall::parse(INPUT.lines());
-    let mut severity = 0;
-    for position in 0..=firewall.size() {
-        severity += firewall.score(position);
-        firewall.update();
-    }
-    println!("Day 13, part 1: {}", severity);
-}
-
-#[derive(Debug)]
-struct Attempt {
-    current_depth: usize,
-    score: u32,
-}
-
-impl Attempt {
-    fn new() -> Attempt {
-        Attempt {
-            current_depth: 0,
-            score: 0,
-        }
-    }
+    let firewall = Firewall::parse(INPUT.lines());
+    println!("Day 13, part 1: {}", firewall.score(0));
 }
 
 #[test]
 fn part2() {
-    let mut firewall = Firewall::parse(INPUT.lines());
-    let mut attempts: Vec<Attempt> = vec![Attempt::new()];
-    let size = firewall.size();
-    (0..10).for_each(|_| firewall.update());
-    while !attempts
-        .iter()
-        .any(|a| a.current_depth > firewall.size() && a.score == 0)
-    {
-        for attempt in attempts.iter_mut().rev().take(size + 1) {
-            attempt.score += firewall.score(attempt.current_depth);
-            attempt.current_depth += 1;
-        }
-        attempts.push(Attempt::new());
-        firewall.update();
-        println!("{}", attempts.len());
+    let firewall = Firewall::parse(INPUT.lines());
+    let mut picosecond = 10;
+    while firewall.score(picosecond) > 0 {
+        picosecond += 1;
     }
-    for (index, attempt) in attempts.iter().enumerate() {
-        println!("{}: {:?}", index + 10, attempt);
-    }
+    println!("Day 13, part 2: {}", picosecond);
 }
